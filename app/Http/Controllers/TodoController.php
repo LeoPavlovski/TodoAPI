968e1a93ea1aaf9f3f2beda2e5b7b3eb64eb2f6c
LeoPavlovski <lp29379@seeu.edu.mk>
2024-02-28 23:53:41 +0100

    todo resource added

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\todoResource;
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     // We are going to write functions here for the crud operations.
+      public function index(){
+            $todos = Todo::all();
+            return todoResource::collection($todos);
+    }
+
 }
