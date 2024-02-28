caedc114076d6da685bc3aa6bf0baa3d8be5fed8
LeoPavlovski <lp29379@seeu.edu.mk>
2024-02-28 23:26:50 +0100

    Added Table, Controler, model

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    // We are going to write functions here for the crud operations.
+}
